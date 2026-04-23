8d4d71c2876800508f91765bd77015d518d1a461
dannaochoa-cloud <danna.ochoa@udea.edu.co>
2026-04-22 23:57:30 -0500

    Implementación de la clase Jugador
    Implementación de la clase Jugador incluyendo gestión manual de memoria para los atributos nombre y apellido mediante punteros char*. Se aplica constructor de copia, operador de asignación y destructor para evitar fugas de memoria y errores de doble liberación. Además, se incorporan funciones auxiliares para la copia segura de cadenas y la generación automática de nombres basados en el número de camiseta, junto con setters que garantizan liberación previa de memoria. De igual forma, se incluye un método para estimar el consumo de memoria del objeto.

---
+++ Jugador.h
@@ -0,0 +1,41 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include "EstadisticasJugador.h"
+
+class Jugador {
+private:
+    int numeroCamiseta;
+    char* nombre;
+    char* apellido;
+    EstadisticasJugador estadisticas;
+
+    // Utilidades privadas para manejo de memoria
+    char* copiarCadena(const char* origen) const;
+    char* generarTexto(const char* prefijo, int numero);
+
+public:
+    Jugador();               // Constructor vacío
+    Jugador(int camiseta);    // Constructor de autogeneración
+
+    // Regla de los 3: Manejo seguro de memoria dinámica
+    Jugador(const Jugador& otro);
+    Jugador& operator=(const Jugador& otro);
+    ~Jugador();
+
+
+    int getNumeroCamiseta() const     { return numeroCamiseta; }
+    const char* getNombre() const     { return nombre; }
+    const char* getApellido() const   { return apellido; }
+    EstadisticasJugador& getEstadisticas() { return estadisticas; }
+
+
+    void setNumeroCamiseta(int num)   { numeroCamiseta = num; }
+    void setNombre(const char* n);
+    void setApellido(const char* a);
+
+    //  Medición de memoria
+    int getMemoriaBytes() const;
+};
+
+#endif
